3aacda8c685aa4485bc02d9fdadbf9e04f8e5fec
Nimesh Thapar <thaparnimesh21@gmail.com>
2022-06-15 13:01:00 +0530

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Playlist from './pages/Playlist';
-import MusicPlayer from './pages/MusicPlayer';
 import Layout from './components/Layout/Layout';
 import { Routes, Route } from 'react-router-dom';
-import { useContext, useEffect } from 'react';
+import { lazy, Suspense, useContext, useEffect } from 'react';
 import { AuthContext } from './store/auth';
 import NotFound from './pages/NotFound';
 import { useLocation } from 'react-router-dom';
+import Loading from './components/Loading/Loading';
+
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login'));
+const Playlist = lazy(() => import('./pages/Playlist'));
+const MusicPlayer = lazy(() => import('./pages/MusicPlayer'));
 
 function App() {
 	const { login, isLoggedIn } = useContext(AuthContext);
@@ -33,7 +35,19 @@ function App() {
 		</Routes>
 	);
 
-	return <Layout>{isLoggedIn ? otherRoutes : loginRoutes}</Layout>;
+	return (
+		<Layout>
+			<Suspense
+				fallback={
+					<div className="center">
+						<Loading white={true} />
+					</div>
+				}
+			>
+				{isLoggedIn ? otherRoutes : loginRoutes}
+			</Suspense>
+		</Layout>
+	);
 }
 
 export default App;
