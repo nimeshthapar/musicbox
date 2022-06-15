71e09fab13825debdef6a3a78151de108b7a3bbf
Nimesh Thapar <thaparnimesh21@gmail.com>
2022-06-15 12:41:23 +0530

    env

---
+++ src/util/spotify.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const redirectedUrl = 'http://localhost:3000';
+const cliendId = `${process.env.REACT_APP_CLIENT_ID}`;
+const authEndpoint = 'https://accounts.spotify.com/authorize?';
+const scopes = ['playlist-read-private', 'user-library-read'];
+
+export const loginEndPoint = `${authEndpoint}client_id=${cliendId}&redirect_uri=${redirectedUrl}&scope=${scopes.join(
+	'%20'
+)}&response_type=token`;
+
+const apiClient = axios.create({
+	baseURL: 'https://api.spotify.com/v1/',
+});
+
+export const setClientToken = (token: string) => {
+	apiClient.interceptors.request.use(async function (config) {
+		config.headers!.Authorization = 'Bearer ' + token;
+		return config;
+	});
+};
+
+export default apiClient;
